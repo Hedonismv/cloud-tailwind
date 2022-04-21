500afa154330c0f6d495cd8218f0e80b0e7914ec
Vitaly Klyhun <klygun90@gmail.com>
2022-04-21 17:57:50 +0300

    App structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Pricing from "./Components/Pricing";
+import Support from "./Components/Support";
 
 function App() {
-  return (
-      <>
-        <Header/>
-      </>
-  );
+    return (
+        <>
+            <Header/>
+            <Hero/>
+            <About/>
+            <Support />
+            <Pricing />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
